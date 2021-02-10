73df022b158c1e2763c4e878eb3c8f93e4dca2d2
Mark <mark@mahernation.com>
2021-02-10 14:26:06 -0500

    Added an IPlayer & Link class, added in a small amount of skeletal stuff/thoughts on setting this stuff up so we have a little bit to run on going into after-class work.

--- Classes/Controllers/StateMachine.cs
+++ Classes/Controllers/StateMachine.cs
@@ -4,11 +4,18 @@ using System.Text;
 
 namespace CSE3902_Game_Sprint0.Classes
 {
-    class StateMachine
+    public class StateMachine
     {
 
         // comment
 
+        //Starting condition should be bomb
+        private int direction = 270;
+        //enum for item selected?
+        // private Tool = bomb or something
+        private bool useTool = false;
+        private bool useSword = false;
 
+        //TODO setup more initial states we think we may need & method bodies for adjusting them when called by Link.cs
     }
 }
---
+++ Classes/Link.cs
@@ -0,0 +1,48 @@
+﻿using CSE3902_Game_Sprint0.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE3902_Game_Sprint0.Classes
+{
+    public class Link : IPlayer
+    {
+        private StateMachine linkState;
+
+        //Initialize Link's default state(s) in a new stateMachine
+        public Link(EeveeSim game)
+        {
+            linkState = new StateMachine();
+        }
+
+        //Sets direction of Link's sprite (0, 90, 180, 270 degrees moving counter-clockwise from east)
+        public void setDirection()
+        {
+            //Call statemachine & have it adjust it adjust stored direction
+        }
+
+        //Set Link to be using an item
+        public void useItem()
+        {
+            //Call statemachine & have it use its currently selected item
+        }
+
+        //Set Link to be using his sword
+        public void useSword()
+        {
+            //Call statemachine & have it use Link's sword
+        }
+
+        //TODO more state altering 
+
+        public void update()
+        {
+
+        }
+
+        public void draw()
+        {
+
+        }
+    }
+}
