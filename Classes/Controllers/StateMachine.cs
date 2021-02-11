c3da9a9bc231c08f7a677b96e4abc5b2aca566b8
Mark <mark@mahernation.com>
2021-02-11 15:31:31 -0500

    Adjusted Link to store game & StateMachine + LinkSpriteFactory constructors to just take Link & then grab game from there.

--- Classes/Controllers/StateMachine.cs
+++ Classes/Controllers/StateMachine.cs
@@ -22,11 +22,11 @@ namespace CSE3902_Game_Sprint0.Classes
         private bool useTool = false;
         private bool useSword = false;
 
-        public StateMachine(EeveeSim game, Link link)
+        public StateMachine(Link link)
         {
-            this.game = game;
+            game = link.game;
             this.link = link;
-            spriteFactory = new LinkSpriteFactory(game, link);
+            spriteFactory = new LinkSpriteFactory(link);
         }
 
         public void MoveUp()
