5c8be2754966beec537bf76ffa8086f67b9acc58
Andrew <lewandowski.107@buckeyemail.osu.edu>
2021-02-13 15:31:36 -0500

    test commit

--- Classes/SpriteFactories/LinkSpriteFactory.cs
+++ Classes/SpriteFactories/LinkSpriteFactory.cs
@@ -44,7 +44,7 @@ namespace CSE3902_Game_Sprint0.Classes.SpriteFactories
         public void movingUp()
         {
             // set new spriteIndex
-            // update link.LinkSprite = new AnimatedSprite
+            // update link.LinkSprite = new AnimatedSprite 
         }
         public void movingDown()
         {
