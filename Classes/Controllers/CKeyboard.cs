b89c9a3305a8554c5326df5a6fd793fcaeb16f70
Mark <mark@mahernation.com>
2021-02-19 13:18:03 -0500

    Made adjustments to the update method of CKeyboard to resolve the movement issue with Link from last push. Link now has a kind of "queue" of directions he's moving in. If you hold multiple movement keys at once, he will move in the last one pressed, if you release the last one pressed, he will begin moving in the second to last button pressed, then third to last & so on.

--- Classes/Controllers/CKeyboard.cs
+++ Classes/Controllers/CKeyboard.cs
@@ -15,6 +15,8 @@ namespace CSE3902_Game_Sprint0
         private StateMachine linkState;
         Dictionary<Keys, ICommand> keyBinds = new Dictionary<Keys, ICommand>();
         HashSet<Keys> heldKeys = new HashSet<Keys>();
+        Dictionary<Keys, int> movementKeys = new Dictionary<Keys, int>();
+
        
         public CKeyboard(EeveeSim game)
         {
@@ -74,27 +76,110 @@ namespace CSE3902_Game_Sprint0
         public void Update()
         {
             KeyboardState keyState = Keyboard.GetState();
+            //Set of all keys released on this update (begins as copy of keys held down)
             HashSet<Keys> releasedKeys = new HashSet<Keys>(heldKeys);
+            //Array of all keys pressed this update
             Keys[] pressedKeys = keyState.GetPressedKeys();
+            int lastMoveKeyCount = movementKeys.Count;
 
+            //For every key pressed on this update-
             for (int i = 0; i < pressedKeys.Length; i++)
             {
-
+                //Removing keys being held still from the releasedKeys set
                 releasedKeys.Remove(pressedKeys[i]);
 
+                //If there is a keyBind for the currently pressed key && it is not in heldKeys set (as in, it has not already been executed), execute the associated command & add the key to heldKeys
                 if (keyBinds.ContainsKey(pressedKeys[i]) && !heldKeys.Contains(pressedKeys[i]))
                 {
                    keyBinds[pressedKeys[i]].Execute();
                     heldKeys.Add(pressedKeys[i]);
+
+                    //If any movement key is being pressed, add it to movementKeys & incrememt key priority of all entries by 1
+                    if (pressedKeys[i] == Keys.W || pressedKeys[i] == Keys.A || pressedKeys[i] == Keys.S || pressedKeys[i] == Keys.D)
+                    {
+                        movementKeys.Add(pressedKeys[i], 0);
+
+                        if (movementKeys.ContainsKey(Keys.W))
+                        {
+                            movementKeys[Keys.W] = movementKeys[Keys.W] + 1;
+                        }
+                        if (movementKeys.ContainsKey(Keys.A))
+                        {
+                            movementKeys[Keys.A] = movementKeys[Keys.A] + 1;
+                        }
+                        if (movementKeys.ContainsKey(Keys.S))
+                        {
+                            movementKeys[Keys.S] = movementKeys[Keys.S] + 1;
+                        }
+                        if (movementKeys.ContainsKey(Keys.D))
+                        {
+                            movementKeys[Keys.D] = movementKeys[Keys.D] + 1;
+                        }
+                    }
                 }
             }
 
+            //For each key released on this update, remove it from heldKeys set
             foreach (Keys releasedKey in releasedKeys)
             {
                 heldKeys.Remove(releasedKey);
+                
+                //If a movement key has been removed, raise priority of all keys with lower priority than the removed key
+                if (movementKeys.ContainsKey(releasedKey))
+                {
+                    int movePriority = movementKeys[releasedKey];
+
+                    if (movementKeys.ContainsKey(Keys.W) && movementKeys[Keys.W] > movePriority)
+                    {
+                        movementKeys[Keys.W] = movementKeys[Keys.W] - 1;
+                    }
+                    if (movementKeys.ContainsKey(Keys.A) && movementKeys[Keys.A] > movePriority)
+                    {
+                        movementKeys[Keys.A] = movementKeys[Keys.A] - 1;
+                    }
+                    if (movementKeys.ContainsKey(Keys.S) && movementKeys[Keys.S] > movePriority)
+                    {
+                        movementKeys[Keys.S] = movementKeys[Keys.S] - 1;
+                    }
+                    if (movementKeys.ContainsKey(Keys.D) && movementKeys[Keys.D] > movePriority)
+                    {
+                        movementKeys[Keys.D] = movementKeys[Keys.D] - 1;
+                    }
+
+                    movementKeys.Remove(releasedKey);
+                }
             }
 
-            if (!heldKeys.Contains(Keys.W) && !heldKeys.Contains(Keys.A) && !heldKeys.Contains(Keys.S) && !heldKeys.Contains(Keys.D))
+            //If a direction is being moved in, adjust link's direction & "queue" if a movement key has been released
+            if (movementKeys.Count > 0)
+            {
+                int newMoveKeyCount = movementKeys.Count;
+                Keys currentMoveKey;
+
+                if (movementKeys.ContainsKey(Keys.W) && movementKeys[Keys.W] == 1)
+                {
+                    currentMoveKey = Keys.W;
+                }
+                else if (movementKeys.ContainsKey(Keys.A) && movementKeys[Keys.A] == 1)
+                {
+                    currentMoveKey = Keys.A;
+                }
+                else if (movementKeys.ContainsKey(Keys.S) && movementKeys[Keys.S] == 1)
+                {
+                    currentMoveKey = Keys.S;
+                }
+                else
+                {
+                    currentMoveKey = Keys.D;
+                }
+                
+                //We need to execute a new movement ONCE, to handle this we only execute a new direction when the movement keys decreases
+                if (newMoveKeyCount < lastMoveKeyCount)
+                {
+                    keyBinds[currentMoveKey].Execute();
+                }
+            }
+            else
             {
                 //New Idle command
                 new IdleLink(linkState).Execute();
