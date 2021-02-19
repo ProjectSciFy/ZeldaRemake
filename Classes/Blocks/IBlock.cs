c379b6ba17c16cac161b53a234f5ee922433d81f
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-02-19 16:08:47 -0500

    Created Block class with list or blocks and IBlock interface.

---
+++ Classes/Blocks/IBlock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace CSE3902_Game_Sprint0.Classes.Blocks
+{
+    interface IBlock
+    {
+        void update();
+
+        void draw();
+    }
+}
