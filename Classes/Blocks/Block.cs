adf040492d7da04c624a4a87f12f91d28ed5ab74
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-02-21 14:35:06 -0500

    Changed block.cs and tilesSpriteFactory and added 2 commands, one for incrementing current block and one for decrementing it.

--- Classes/Blocks/Block.cs
+++ Classes/Blocks/Block.cs
@@ -8,29 +8,19 @@ namespace CSE3902_Game_Sprint0.Classes.Blocks
     public class Block : IBlock
     {
         public EeveeSim game;
-        private List<Block> blocks = new List<Block>();
-        private int currentBlockIndex = 0;
         private TilesSpriteFactory spriteFactory;
         public Vector2 drawLocation;
         public ISprite blockSprite;
-        public Vector2 blockSize = new Vector2(5, 5);
         public Vector2 velocity = new Vector2(0, 0);
+        // UPDATE SIZE
+        public Vector2 blockSize = new Vector2(5, 5);
+        
 
         public Block(EeveeSim game, Vector2 drawHere)
         {
             this.game = game;
             spriteFactory = game.tileSpriteFactory;
             drawLocation = drawHere;
-        }
-
-        public void setCurrentBlockIndex(int newBlockIndex)
-        {
-            this.currentBlockIndex = newBlockIndex;
-        }
-
-        public int getCurrentBlockIndex()
-        {
-            return currentBlockIndex;
         }
 
         public void update()
---
+++ Classes/Controllers/BlockCommands/DecrementBlock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE3902_Game_Sprint0.Classes.Controllers.BlockCommands
+{
+    public class DecrementBlock : ICommand
+    {
+        private TilesSpriteFactory blockFactory;
+        public DecrementBlock(TilesSpriteFactory blockFactory)
+        {
+            this.blockFactory = blockFactory;
+        }
+        public void Execute()
+        {
+            if (this.blockFactory.currentBlock > 0)
+            {
+                this.blockFactory.currentBlock--;
+            }
+            else //first block in list, currentBlock == 0, cycle to last in list, index 9
+            {
+                this.blockFactory.currentBlock = 9;
+            }
+        }
+    }
+}
