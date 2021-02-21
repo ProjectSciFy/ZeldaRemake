02f4708ef3f686f1bb9c902a5348c6d22e85b911
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-02-21 17:33:01 -0500

    changed tile sprite factory

--- Classes/Blocks/TilesSpriteFactory.cs
+++ Classes/Blocks/TilesSpriteFactory.cs
@@ -28,42 +28,43 @@ namespace CSE3902_Game_Sprint0
         // [8]  [9]
         public void Floor()
         {
-            this.currentBlock = newBlock;
-            switch (newBlock)
-            {
-                case 0:
-                    block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(984, 11, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1));
-                    break;
-                case 1:
-                    block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1001, 11, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1));
-                    break;
-                case 2:
-                    block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1018, 11, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1));
-                    break;
-                case 3:
-                    block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1035, 11, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1));
-                    break;
-                case 4:
-                    block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(984, 28, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1));
-                    break;
-                case 5:
-                    block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1001, 28, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1));
-                    break;
-                case 6:
-                    block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1018, 28, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1));
-                    break;
-                case 7:
-                    block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1035, 28, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1));
-                    break;
-                case 8:
-                    block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(984, 45, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1));
-                    break;
-                case 9:
-                    block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1001, 45, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1));
-                    break;
-                default:
-                    break;
-            }
+            block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(984, 11, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1), 10);
+        }
+        public void Wall()
+        {
+            block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1001, 11, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1), 10);
+        }
+        public void Boss1Tile()
+        {
+            block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1018, 11, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1), 10);
+        }
+        public void Boss2Tile()
+        {
+            block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1035, 11, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1), 10);
+        }
+        public void Void()
+        {
+            block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(984, 28, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1), 10);
+        }
+        public void Texture()
+        {
+            block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1001, 28, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1), 10);
+        }
+        public void Empty()
+        {
+            block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1018, 28, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1), 10);
+        }
+        public void Stairs()
+        {
+            block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1035, 28, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1), 10);
+        }
+        public void Bricks()
+        {
+            block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(984, 45, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1), 10);
+        }
+        public void DungeonWall()
+        {
+            block.blockSprite = new UniversalSprite(game, tileSpriteSheet, new Rectangle(1001, 45, 16, 16), Color.White, SpriteEffects.None, new Vector2(1, 1), 10);
         }
     }
 }
