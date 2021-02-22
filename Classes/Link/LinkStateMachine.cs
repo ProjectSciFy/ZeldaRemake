edc7e98672a4e045b98e15b2681b990c2da8a8d3
Jared <zahn.57@buckeyemail.osu.edu>
2021-02-22 16:20:44 -0600

    LinkStateMachine updates to weapon animations.

--- Classes/Link/LinkStateMachine.cs
+++ Classes/Link/LinkStateMachine.cs
@@ -342,7 +342,7 @@ namespace CSE3902_Game_Sprint0.Classes
             {
                 if (useSword)
                 {
-                    //Sword();
+                    this.Sword();
                 }
                 else if (useBomb)
                 {
