9145184213a26cf76b640651fa9e09e84905b19e
Jared <zahn.57@buckeyemail.osu.edu>
2021-02-21 22:02:01 -0600

    Link Damaged State.

--- Classes/Controllers/CKeyboard.cs
+++ Classes/Controllers/CKeyboard.cs
@@ -48,7 +48,7 @@ namespace CSE3902_Game_Sprint0
             keyBinds.Add(Keys.N, new DrawSprite(game, game.eeveeTexture, game.eeveeSprite, game.eeveeLocation = new Vector2((game.GraphicsDevice.Viewport.Bounds.Width / 2) - (25 / 2), (game.GraphicsDevice.Viewport.Bounds.Height / 2) - (21 / 2)), new Vector2(2, 0), new Rectangle(75, 48, 25, 21), Color.White, SpriteEffects.None, new Vector2(1, 3)));
 
             //E -- test damage taken animation
-            keyBinds.Add(Keys.E, new DrawSprite(game, game.eeveeTexture, game.eeveeSprite, game.eeveeLocation = new Vector2((game.GraphicsDevice.Viewport.Bounds.Width / 2) - (24 / 2), (game.GraphicsDevice.Viewport.Bounds.Height / 2) - (25 / 2)), new Vector2(0, 2), new Rectangle(0, 112, 24, 25), Color.White, SpriteEffects.None, new Vector2(1, 1)));
+            keyBinds.Add(Keys.E, new DamagedLink(linkState, linkState.direction));
 
             //1, 2, 3, 4 -- change between items animation
             keyBinds.Add(Keys.D1, new ItemLink(linkState, LinkStateMachine.Item.sword, linkState.direction)); // sword item
---
+++ Classes/Controllers/LinkCommands/DamagedLink.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE3902_Game_Sprint0.Classes.Controllers.LinkCommands
+{
+    class DamagedLink : ICommand
+    {
+        private LinkStateMachine linkState;
+        private LinkStateMachine.Direction direction;
+
+        public DamagedLink(LinkStateMachine linkState, LinkStateMachine.Direction direction)
+        {
+            this.linkState = linkState;
+            this.direction = direction;
+        }
+        public void Execute()
+        {
+            linkState.moving = false;
+
+            switch (direction)
+            {
+                case LinkStateMachine.Direction.up:
+                    linkState.Damaged();
+                    break;
+
+                case LinkStateMachine.Direction.down:
+                    linkState.Damaged();
+                    break;
+
+                case LinkStateMachine.Direction.left:
+                    linkState.Damaged();
+                    break;
+
+                case LinkStateMachine.Direction.right:
+                    linkState.Damaged();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
