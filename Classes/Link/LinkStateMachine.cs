1fc5af7357e8b86178b963b3c2122a8de56f6f01
Jared <zahn.57@buckeyemail.osu.edu>
2021-02-22 20:36:14 -0600

    BombStateMachine created and bomb.cs edits

--- Classes/Link/LinkStateMachine.cs
+++ Classes/Link/LinkStateMachine.cs
@@ -219,6 +219,7 @@ namespace CSE3902_Game_Sprint0.Classes
                         {
                             currentState = CurrentState.bombRight;
                             spriteFactory.BombRight();
+
                         }
                         break;
 
--- Classes/Projectiles/Bomb.cs
+++ Classes/Projectiles/Bomb.cs
@@ -22,6 +22,7 @@ namespace CSE3902_Game_Sprint0.Classes.Projectiles
         public Vector2 spawnLocation;
         public Vector2 spriteSize = new Vector2(16, 16);
         private Bomb bomb;
+        private BombStateMachine myState;
 
         public Bomb(EeveeSim game, Link link, LinkStateMachine linkState)
         {
@@ -32,6 +33,7 @@ namespace CSE3902_Game_Sprint0.Classes.Projectiles
         }
         public void Update()
         {
+            myState.Update();
             bombSprite.Update();
         }
 
