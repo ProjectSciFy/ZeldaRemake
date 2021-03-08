3686f9acb38b61682f6133a33a69f8694a6689b3
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-03-08 15:54:28 -0500

    Created a map control area, file for specific map control (to check doors) -- WE MIGHT NOT NEED MATRIX SINCE WE HAVE B/L/R/T DOORS IN ROOM FILE? -- also created parser file, need to complete; removed all room stuff from eeveesim game file.

--- EeveeSim.cs
+++ EeveeSim.cs
@@ -62,10 +62,6 @@ namespace CSE3902_Game_Sprint0
 
         public CollisionManager collisionManager;
 
-        // ROOMS
-        private int[][] map = new int[8][];
-        private Vector2 mapSize = new Vector2(8, 8);
-
         public EeveeSim()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -127,27 +123,6 @@ namespace CSE3902_Game_Sprint0
             currentEnemy = Enemies.Stalfos;
             drawnEnemy = new EnemyStalfos(this, new Vector2(400, 100));
 
-            /*
-             * MAP AND ROOM STUFF
-             */
-            //0 if no room, othersize room number (starts from bottom, left to right)
-            // 0  0  0  0  0  0  0  0
-            // 0  16 17 18 0  0  0  0
-            // 0  0  0  13 0  14 15 0
-            // 0  8  9  10 11 12 0  0
-            // 0  0  5  6  7  0  0  0
-            // 0  0  0  4  0  0  0  0
-            // 0  0  1  2  3  0  0  0
-            // 0  0  0  0  0  0  0  0
-            this.map[0][0] = 0;  this.map[0][1] = 0;  this.map[0][2] = 0;  this.map[0][3] = 0;  this.map[0][4] = 0;  this.map[0][5] = 0;  this.map[0][6] = 0;  this.map[0][7] = 0;
-            this.map[1][0] = 0;  this.map[1][1] = 16; this.map[1][2] = 17; this.map[1][3] = 18; this.map[1][4] = 0;  this.map[1][5] = 0;  this.map[1][6] = 0;  this.map[1][7] = 0;
-            this.map[2][0] = 0;  this.map[2][1] = 0;  this.map[2][2] = 0;  this.map[2][3] = 13; this.map[2][4] = 0;  this.map[2][5] = 14; this.map[2][6] = 15; this.map[2][7] = 0;
-            this.map[3][0] = 0;  this.map[3][1] = 8;  this.map[3][2] = 9;  this.map[3][3] = 10; this.map[3][4] = 11; this.map[3][5] = 12; this.map[3][6] = 0;  this.map[3][7] = 0;
-            this.map[4][0] = 0;  this.map[4][1] = 0;  this.map[4][2] = 5;  this.map[4][3] = 6;  this.map[4][4] = 7;  this.map[4][5] = 0;  this.map[4][6] = 0;  this.map[4][7] = 0;
-            this.map[5][0] = 0;  this.map[5][1] = 0;  this.map[5][2] = 0;  this.map[5][3] = 4;  this.map[5][4] = 0;  this.map[5][5] = 0;  this.map[5][6] = 0;  this.map[5][7] = 0;
-            this.map[6][0] = 0;  this.map[6][1] = 0;  this.map[6][2] = 1;  this.map[6][3] = 2;  this.map[6][4] = 3;  this.map[6][5] = 0;  this.map[6][6] = 0;  this.map[6][7] = 0;
-            this.map[7][0] = 0;  this.map[7][1] = 0;  this.map[7][2] = 0;  this.map[7][3] = 0;  this.map[7][4] = 0;  this.map[7][5] = 0;  this.map[7][6] = 0;  this.map[7][7] = 0;
-
             controllerList.Add(new CKeyboard(this));
             controllerList.Add(new CMouse(this));
 
@@ -174,20 +149,6 @@ namespace CSE3902_Game_Sprint0
             eeveeLocation = new Vector2((GraphicsDevice.Viewport.Bounds.Width / 2) - (21 / 2), (GraphicsDevice.Viewport.Bounds.Height / 2) - (24 / 2));
             eeveeSprite = new StaticSprite(this, eeveeTexture, eeveeLocation, eevelocity, new Rectangle(21, 0, 21, 24), Color.White, SpriteEffects.None);
             credits = Content.Load<SpriteFont>("Credits");
-        }
-
-
-        /*
-         * MAP AND ROOM GET METHODS
-         */
-        public int[][] getMap()
-        {
-            return this.map;
-        }
-
-        public Vector2 getMapSize()
-        {
-            return this.mapSize;
         }
 
 
