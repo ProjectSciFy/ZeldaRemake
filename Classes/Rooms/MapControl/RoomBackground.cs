6e05b9ae966ba4a8d87244ec88bebc4d0f421db3
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-03-13 12:38:00 -0500

    Added stuff to rooms

---
+++ Classes/Rooms/MapControl/RoomBackground.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE3902_Game_Sprint0.Classes.Rooms.MapControl
+{
+    class RoomBackground
+    {
+        private int screenWidth;
+        private int screenLength;
+
+        private int doorSideLength;
+
+        private Rectangle botDoorLoc;
+        private Rectangle leftDoorLoc;
+        private Rectangle rightDoorLoc;
+        private Rectangle topDoorLoc;
+
+        public RoomBackground(EeveeSim game)
+        {
+
+        }
+    }
+}
--- EeveeSim.cs
+++ EeveeSim.cs
@@ -181,7 +181,7 @@ namespace CSE3902_Game_Sprint0
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
 
