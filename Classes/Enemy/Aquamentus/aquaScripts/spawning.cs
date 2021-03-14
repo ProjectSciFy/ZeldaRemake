a9967308da5cc2c598950b0ae2ac245a376a4b75
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-03-14 18:51:56 -0400

    capitalization

--- Classes/Enemy/Aquamentus/aquaScripts/spawning.cs
+++ Classes/Enemy/Aquamentus/aquaScripts/spawning.cs
@@ -1,12 +1,10 @@
-using CSE3902_Game_Sprint0.Classes.Projectiles;
-using CSE3902_Game_Sprint0.Classes.SpriteFactories;
-using Microsoft.Xna.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace CSE3902_Game_Sprint0.Classes.Enemy.Aquamentus.aquaScripts
+namespace CSE3902_Game_Sprint0.Classes.Enemy.Aquamentus.AquaScripts
 {
-    
-
+    class Spawning
+    {
+    }
 }
