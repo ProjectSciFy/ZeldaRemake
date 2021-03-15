0212781b81ee3f2339f0db6faad0705edc7a2d68
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-03-15 15:46:49 -0400

    changed graphics

--- Classes/Enemy/Stalfos/StalfosStateMachine.cs
+++ Classes/Enemy/Stalfos/StalfosStateMachine.cs
@@ -121,6 +121,7 @@ namespace CSE3902_Game_Sprint0.Classes._21._2._13
                             direction = Direction.down;
                             break;
                     }
+                    moving = !moving;
                 }
                 else
                 {
