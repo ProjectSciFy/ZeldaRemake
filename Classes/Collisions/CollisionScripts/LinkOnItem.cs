c84cb84d0cf7b3d47b8b7da0c3c480ddfbb9bb4a
Mark <mark@mahernation.com>
2021-03-15 21:49:36 -0400

    Commiting WIP changes to BlueRupee & Collision code for items for merge.

---
+++ Classes/Collisions/CollisionScripts/LinkOnItem.cs
@@ -0,0 +1,28 @@
+﻿using CSE3902_Game_Sprint0.Classes.Items;
+using CSE3902_Game_Sprint0.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE3902_Game_Sprint0.Classes.Collisions.CollisionScripts
+{
+    public class LinkOnItem : ICommand
+    {
+        private Link link;
+        private IItem item;
+        private Collision.Collision.Direction direction;
+
+        public LinkOnItem(Link link, IItem item, Collision.Collision.Direction direction)
+        {
+            this.link = link;
+            this.item = item;
+            this.direction = direction;
+        }
+        
+        public void Execute()
+        {
+            link.game.collisionManager.collisionEntities.Remove((ICollisionEntity)item);
+            //TODO - DELETE THE ITEM FROM WHEREVER IT IS BEING STORED!!!
+        }
+    }
+}
