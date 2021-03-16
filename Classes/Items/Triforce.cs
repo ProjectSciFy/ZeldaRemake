5c4224e3e8a7c32bf0d870823aefde0b902410fa
Jared <zahn.57@buckeyemail.osu.edu>
2021-03-15 21:44:13 -0500

    TriForce.cs edit

--- Classes/Items/Triforce.cs
+++ Classes/Items/Triforce.cs
@@ -12,7 +12,7 @@ namespace CSE3902_Game_Sprint0.Classes.Items
         private ZeldaGame game;
         private ISprite itemSprite;
         private ItemSpriteFactory itemFactory;
-        public Vector2 spriteSize = new Vector2(8, 16);
+        public Vector2 spriteSize = new Vector2(10, 10);
         public Rectangle hitbox = new Rectangle(0, 0, 0, 0);
         public Vector2 position;
         public Vector2 drawLocation;
