661e8b6dee7337f76b31c2dd3b30aef4f23bd22c
Jared <zahn.57@buckeyemail.osu.edu>
2021-03-19 15:13:52 -0500

    edits to file descriptions.

--- Classes/Level/Background.cs
+++ Classes/Level/Background.cs
@@ -20,6 +20,8 @@ namespace CSE3902_Game_Sprint0.Classes.Level
         private int windowWidth;
         private int windowHeight;
         private Texture2D itemSpriteSheet;
+        private int roomLimiter;
+        private int drawOffset;;
         public Background(ZeldaGame game, int roomNumber)
         {
             this.game = game;
@@ -32,11 +34,14 @@ namespace CSE3902_Game_Sprint0.Classes.Level
             int windowHeightFloor = (windowHeight/3 - 176/3)/2;
             int windowWidthFloor = (windowWidth/3 - 256/3)/2;
 
-            drawLocationInterior = new Vector2(windowWidthFloor + 96, windowHeightFloor + 96);
+            roomLimiter = 10;
+            drawOffset = 96;
+
+            drawLocationInterior = new Vector2(windowWidthFloor + drawOffset, windowHeightFloor + drawOffset);
             drawLocationExterior = new Vector2(windowWidthFloor, windowHeightFloor);
 
             roominterior = roomTextures.getRoom(roomNumber);
-            roomexterior = new UniversalSprite(game, itemSpriteSheet, new Rectangle(521, 11, 256, 176), Color.White, SpriteEffects.None, new Vector2(1, 1), 10,0.0f);
+            roomexterior = new UniversalSprite(game, itemSpriteSheet, new Rectangle(521, 11, 256, 176), Color.White, SpriteEffects.None, new Vector2(1, 1), roomLimiter,0.0f);
 
             
         }
