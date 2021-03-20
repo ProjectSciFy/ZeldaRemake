961d66a703728e01f5a1868861d199eef9d79674
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-03-19 22:07:51 -0400

    added cam file

---
+++ Classes/Level/Camera.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE3902_Game_Sprint0.Classes.Level
+{
+    public class Camera
+    {
+        public Camera()
+        {
+
+        }
+    }
+}
