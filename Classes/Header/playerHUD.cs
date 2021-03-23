2cd5a561cfaa06fe7e22a67250154f79b7b77fff
Devan <devan.szpak@gmail.com>
2021-03-23 15:53:02 -0500

    setup playerHUD and its sprite factory

--- Classes/Header/playerHUD.cs
+++ Classes/Header/playerHUD.cs
@@ -6,7 +6,7 @@ using CSE3902_Game_Sprint0.Classes.SpriteFactories;
 
 namespace CSE3902_Game_Sprint0.Classes.Header
 {
-    class playerHUD
+    public class playerHUD
     {
         private ZeldaGame game;
         private ISprite hudSprite;
