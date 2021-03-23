c81b375aef4bd92fd4e6a274935bf2860f1e5c75
Jared <zahn.57@buckeyemail.osu.edu>
2021-03-23 15:44:59 -0500

    comments added

--- Classes/SpriteFactories/HudSpriteFactory.cs
+++ Classes/SpriteFactories/HudSpriteFactory.cs
@@ -18,10 +18,34 @@ namespace CSE3902_Game_Sprint0.Classes.SpriteFactories
             this.game = game;
             game.spriteSheets.TryGetValue("HUD", out hudSpriteSheet);
         }
-        //this tempelate need life, map, level to be added
+        //this is just the base template sprite. HUD still needs life, map, level, counters to be implemented and updated in real time.
         public UniversalSprite baseHud()
         {
             return new UniversalSprite(game, hudSpriteSheet, new Rectangle(257, 10, 256, 56), Color.White, SpriteEffects.None, new Vector2(1,1), 10, itemDepth);
         }
+
+        //public UniversalSprite mapHUD()
+        //{
+        //}
+
+        //public UniversalSprite keyHUD()
+        //{
+        //}
+
+        //public UniversalSprite bluerupeeHUD()
+        //{
+        //}
+
+        //public UniversalSprite yellowrupeeHUD()
+        //{
+        //}
+
+        //public UniversalSprite lifeHUD()
+        //{
+        //}
+
+        //public UniversalSprite levelHUD()
+        //{
+        //}
     }
 }
