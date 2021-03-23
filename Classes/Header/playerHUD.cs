90c2d2b5a34f8eeef9b9a7526b18d071b066d66d
Jared <zahn.57@buckeyemail.osu.edu>
2021-03-23 14:11:25 -0500

    player hud files created

---
+++ Classes/Header/playerHUD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE3902_Game_Sprint0.Classes.Header
+{
+    class playerHUD
+    {
+        public playerHUD()
+        {
+
+        }
+
+
+    }
+}
