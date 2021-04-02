315f8a9eb93139cbfb1d3df385a4c5fab1b2d409
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-04-01 21:04:30 -0400

    room texture updates

--- Classes/Level/RoomTextureStorage.cs
+++ Classes/Level/RoomTextureStorage.cs
@@ -20,25 +20,25 @@ namespace CSE3902_Game_Sprint0.Classes.Level
 
         public static Dictionary<int, Rectangle> INTERIOR_DIMENSIONS = new Dictionary<int, Rectangle>()
         {
-            [1] = new Rectangle(1, 192, INTERIOR_WIDTH, INTERIOR_LENGTH), //
-            [2] = new Rectangle(196, 192, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            [3] = new Rectangle(391, 192, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            [4] = new Rectangle(586, 192, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            [5] = new Rectangle(586, 192, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            [6] = new Rectangle(781, 192, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            [7] = new Rectangle(976, 192, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            [8] = new Rectangle(976, 882, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            [9] = new Rectangle(1, 307, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            [10] = new Rectangle(196, 307, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            [11] = new Rectangle(781, 192, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            [12] = new Rectangle(391, 307, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            [13] = new Rectangle(586, 307, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            [14] = new Rectangle(781, 307, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            [15] = new Rectangle(976, 307, INTERIOR_WIDTH, INTERIOR_LENGTH),//
-            //[16] = new Rectangle(421, 1009, INTERIOR_WIDTH, INTERIOR_LENGTH),
-            [16] = new Rectangle(1, 192, INTERIOR_WIDTH, INTERIOR_LENGTH),
-            [17] = new Rectangle(1, 422, INTERIOR_WIDTH, INTERIOR_LENGTH),
-            [18] = new Rectangle(196, 422, INTERIOR_WIDTH, INTERIOR_LENGTH),
+            [1] = new Rectangle(290, 918, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [2] = new Rectangle(547, 918, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [3] = new Rectangle(804, 918, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [4] = new Rectangle(547, 741, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [5] = new Rectangle(290, 564, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [6] = new Rectangle(547, 564, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [7] = new Rectangle(804, 564, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [8] = new Rectangle(33, 387, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [9] = new Rectangle(290, 387, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [10] = new Rectangle(547, 387, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [11] = new Rectangle(804, 387, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [12] = new Rectangle(1061, 387, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [13] = new Rectangle(547, 210, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [14] = new Rectangle(1061, 210, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [15] = new Rectangle(1318, 210, INTERIOR_WIDTH, INTERIOR_LENGTH),//
+            [16] = new Rectangle(33, 32, INTERIOR_WIDTH, INTERIOR_LENGTH),
+            //[16] = new Rectangle(290, 918, INTERIOR_WIDTH, INTERIOR_LENGTH),
+            [17] = new Rectangle(290, 33, INTERIOR_WIDTH, INTERIOR_LENGTH),
+            [18] = new Rectangle(547, 33, INTERIOR_WIDTH, INTERIOR_LENGTH),
 
         };
         public static Dictionary<int, Rectangle> DOOR_DIMENSIONS = new Dictionary<int, Rectangle>()
@@ -68,7 +68,7 @@ namespace CSE3902_Game_Sprint0.Classes.Level
         public RoomTextureStorage(ZeldaGame game)
         {
             this.game = game;
-            game.spriteSheets.TryGetValue("DungeonTileset", out roomSpriteSheet);
+            game.spriteSheets.TryGetValue("DungeonBackgrounds", out roomSpriteSheet);
             game.spriteSheets.TryGetValue("DungeonTileset", out tileSpriteSheet);
             game.spriteSheets.TryGetValue("ItemsAndWeapons", out itemSpriteSheet);
 
@@ -83,7 +83,7 @@ namespace CSE3902_Game_Sprint0.Classes.Level
         }
         public UniversalSprite getDoor(int doorValue)
         {
-            return new UniversalSprite(game, roomSpriteSheet, DOOR_DIMENSIONS[doorValue], Color.White, SpriteEffects.None, new Vector2(1, 1), 10, 0.0f);
+            return new UniversalSprite(game, tileSpriteSheet, DOOR_DIMENSIONS[doorValue], Color.White, SpriteEffects.None, new Vector2(1, 1), 10, 0.0f);
         }
     }
 }
