58e6b131b384f173c1747d86df02686e0b54b5e1
Mark <mark@mahernation.com>
2021-04-03 16:21:41 -0400

    Updated Fairy class to not vibrate at dangerous speeds.

--- Classes/Items/Fairy.cs
+++ Classes/Items/Fairy.cs
@@ -63,6 +63,7 @@ namespace CSE3902_Game_Sprint0.Classes.Items
                 default:
                     break;
             }
+            timer = 60;
         }
 
         public Rectangle CollisionRectangle()
