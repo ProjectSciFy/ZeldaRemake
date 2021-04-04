29199670c6aa8759a8d3597a46db5d704d36e869
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-04-04 13:50:42 -0400

    reformatted game class, COMMENTS!

--- ZeldaGame.cs
+++ ZeldaGame.cs
@@ -24,71 +24,76 @@ namespace CSE3902_Game_Sprint0
 
     public class ZeldaGame : Game
     {
+        //GENERAL STUFF
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        //CONTROLLERS
         public List<IController> controllerList = new List<IController>();
-
+        //UTILITY STORAGE
+        public GameUtility util;
+        //SPRITE SHEETS
         public Dictionary<string, Texture2D> spriteSheets = new Dictionary<string, Texture2D>();
+        //SPRITE FACTORIES
         public EnemySpriteFactory enemySpriteFactory;
         public ProjectileSpriteFactory projectileSpriteFactory;
         public HudSpriteFactory hudSpriteFactory;
-
+        //STATE MACHINES
         public LinkStateMachine linkStateMachine;
         public BombStateMachine bombStateMachine;
-
+        //LINK
         public Classes.Link link;
-        public Classes.Projectiles.Bomb bomb;
-
+        //COLLISIONS
         public ProjectileHandler projectileHandler;
         public CollisionManager collisionManager;
-
+        //ROOMS
         public List<Room> roomList;
         public Dictionary<int, int[]> neighbors;
         public Room currentRoom;
         public Room oldRoom;
-
+        //GAME STATES
         public IGameState currentMainGameState;
         public IGameState currentGameState;
 
-        public GameUtility util;
         public ZeldaGame()
         {
             this.util = new GameUtility();
             _graphics = new GraphicsDeviceManager(this);
+            IsMouseVisible = true;
             //SIZE OF SCREEN 
             _graphics.PreferredBackBufferWidth = 1024;
             _graphics.PreferredBackBufferHeight = 896;
+            //DIRECTORY
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
             base.Initialize();
-            //Setting up CollisionManager
+            //COLLISIONS
             collisionManager = new CollisionManager();
-            //Setting up projectileHandler
+            //PROJECTILES
             projectileSpriteFactory = new ProjectileSpriteFactory(this);
             projectileHandler = new ProjectileHandler(this);
-            /* LINK */
-            //set StateMachine and Link to be used:
+            //LINK
             link = new Classes.Link(this);
             linkStateMachine = new LinkStateMachine(link);
-            //link is now created, maintains an instance of StateMachine to be passed around for commands:
             link.SetState(linkStateMachine);
-            //Setting up playerHudSpriteFactory
+            //HUD
             hudSpriteFactory = new HudSpriteFactory(this);
-            //Setting up enemy spritefactory
+            //ENEMIES
             enemySpriteFactory = new EnemySpriteFactory(this);
+            //CONTROLLERS
             controllerList.Add(new CKeyboard(this));
             controllerList.Add(new CMouse(this));
+            //ROOMS
             neighbors = Parser.ParseNeighborCSV();
             roomList = new List<Room>();
-            //18 rooms
+            /* 18 ROOMS */
             for (int i = 1; i < 19; i++)
             {
                 roomList.Add(Parser.ParseRoomCSV(this, i));
             }
+            /* SET CURRENT ROOM TO ROOM WITH CURRENT roomNumber */
             foreach (Room r in roomList)
             {
                 if (r.getRoomNumber() == util.roomNumber)
@@ -97,6 +102,7 @@ namespace CSE3902_Game_Sprint0
                 }
             }
             currentRoom.Initialize();
+            //GAME STATE
             currentMainGameState = new MainState(this, currentRoom);
             currentGameState = currentMainGameState;
         }
@@ -104,7 +110,7 @@ namespace CSE3902_Game_Sprint0
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            //Loading all of our textures into a texture dictionary
+            //SPRITE SHEETS
             spriteSheets.Add("Link", Content.Load<Texture2D>("NES - The Legend of Zelda - Link"));
             spriteSheets.Add("HUD", Content.Load<Texture2D>("NES - The Legend of Zelda - HUD & Pause Screen"));
             spriteSheets.Add("ItemsAndWeapons", Content.Load<Texture2D>("NES - The Legend of Zelda - Items & Weapons"));
@@ -120,18 +126,26 @@ namespace CSE3902_Game_Sprint0
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            //COLLISIONS
             currentGameState.UpdateCollisions();
+            //GENERAL
             base.Update(gameTime);
+            //GAME STATE
             currentGameState.Update();
         }
 
+        /*
+         * Change room smooothly from room number util.roomNumber to room number newRoom
+         */
         public void changeRoom(int newRoom, Collision.Direction direction)
         {
             util.keyPressedTempVariable = true;
             oldRoom = currentRoom;
+            //CLEAR COLLISIONS
             collisionManager.ClearNotLink();
             projectileHandler.Clear();
             util.roomNumber = newRoom;
+            //FIND NEW ROOM
             foreach (Room r in roomList)
             {
                 if (r.getRoomNumber() == newRoom)
@@ -139,15 +153,18 @@ namespace CSE3902_Game_Sprint0
                     currentRoom = r;
                 }
             }
+            //TRANSITION STATE
             currentMainGameState = new MainState(this, currentRoom);
             currentGameState = new TransitionState(this, oldRoom, currentRoom, direction);
         }
 
         protected override void Draw(GameTime gameTime)
         {
+            //BLACK BACKGROUND
             GraphicsDevice.Clear(Color.Black);
-
+            //GENERAL
             base.Draw(gameTime);
+            //GAME STATE
             currentGameState.Draw();
         }
     }
