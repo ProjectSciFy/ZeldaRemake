38c9b5c52cc0a885516a20cc748882e78d95dc2a
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-04-05 14:15:10 -0400

    fixed small background thing

--- Classes/SpriteFactories/HudSpriteFactory.cs
+++ Classes/SpriteFactories/HudSpriteFactory.cs
@@ -88,7 +88,7 @@ namespace CSE3902_Game_Sprint0.Classes.SpriteFactories
         }
         public UniversalSprite left()
         {
-            return new UniversalSprite(game, hudSpriteSheet, new Rectangle(0, 0, 43, 178), Color.Black, SpriteEffects.None, new Vector2(1, 1), 10, 0.0f);
+            return new UniversalSprite(game, hudSpriteSheet, new Rectangle(0, 0, 42, 178), Color.Black, SpriteEffects.None, new Vector2(1, 1), 10, 0.0f);
         }
     }
 }
