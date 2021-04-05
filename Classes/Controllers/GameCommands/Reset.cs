5a74e492fdd45c0817fed027d311a18f042fdae5
Jared <zahn.57@buckeyemail.osu.edu>
2021-04-05 15:42:06 -0500

    hasMap is established in reset command

--- Classes/Controllers/GameCommands/Reset.cs
+++ Classes/Controllers/GameCommands/Reset.cs
@@ -58,6 +58,7 @@ namespace CSE3902_Game_Sprint0.Classes.Controllers.GameCommands
             game.util.numKeys = 0;
             game.util.numBrups = 0;
             game.util.numYrups = 0;
+            game.util.hasMap = false;
             // - Jared will take care of this, planning on implementing a "reset" method inside the playerHUD.cs class
 
             //RE-PARSE ROOMS HERE
