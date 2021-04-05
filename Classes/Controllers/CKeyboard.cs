ae1b9cad7a539aaf630248ba0212c1e6c2b93870
odom4242 <57304360+odom4242@users.noreply.github.com>
2021-04-05 16:53:58 -0400

    update

--- Classes/Controllers/CKeyboard.cs
+++ Classes/Controllers/CKeyboard.cs
@@ -44,8 +44,6 @@ namespace CSE3902_Game_Sprint0
             keyBinds.Add(Keys.N, new SecondaryWeaponLink(linkState));
             keyBinds.Add(Keys.Z, new PrimaryWeaponLink(linkState)); 
 
-            keyBinds.Add(Keys.E, new DamagedLink(linkState));
-
             keyBinds.Add(Keys.D2, new SecondaryWeaponSelect(linkState, LinkStateMachine.Weapon.bomb));
             keyBinds.Add(Keys.NumPad2, new SecondaryWeaponSelect(linkState, LinkStateMachine.Weapon.bomb));
             keyBinds.Add(Keys.D3, new SecondaryWeaponSelect(linkState, LinkStateMachine.Weapon.arrow));
