b4837dbae017d6d5bf893274c81921e512a8e623
Devan <devan.szpak@gmail.com>
2021-04-05 21:07:25 -0500

    add game over text to death state

--- Classes/GameState/DeathState.cs
+++ Classes/GameState/DeathState.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 using CSE3902_Game_Sprint0.Classes.Controllers.GameCommands;
 using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using CSE3902_Game_Sprint0.Classes.Scripts;
 
 namespace CSE3902_Game_Sprint0.Classes.GameState
 {
@@ -10,15 +13,23 @@ namespace CSE3902_Game_Sprint0.Classes.GameState
     {
         private ZeldaGame game;
         private int timer = -1;
+        private ISprite texture;
+        private Texture2D gameOverSpriteSheet;
+        private float itemDepth = 0.4f;
+        private const int HEIGHT = 60;
+        private const int WIDTH = 360;
 
         public DeathState(ZeldaGame game)
         {
             this.game = game;
+            game.spriteSheets.TryGetValue("GameOver", out gameOverSpriteSheet);
+            texture = new UniversalSprite(game, gameOverSpriteSheet, new Rectangle(36, 126, WIDTH, HEIGHT), Color.White, SpriteEffects.None, new Vector2(1, 1), 10, itemDepth);
         }
 
         void IGameState.Draw()
         {
             this.game.link.Draw();
+            texture.Draw(new Vector2(game.GraphicsDevice.Viewport.Width / 3 - WIDTH, game.GraphicsDevice.Viewport.Height / 2 - HEIGHT));
         }
 
         void IGameState.Update()
@@ -31,6 +42,7 @@ namespace CSE3902_Game_Sprint0.Classes.GameState
             }
             game.link.Update();
             game.link.Draw();
+
 
             if (timer > 0)
             {
