a8a4558fb44fc9cdb236daff048f8c7c83cf5403
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-04-18 23:46:48 -0400

    fixed reset issue

--- Classes/Controllers/GameCommands/Reset.cs
+++ Classes/Controllers/GameCommands/Reset.cs
@@ -59,6 +59,8 @@ namespace CSE3902_Game_Sprint0.Classes.Controllers.GameCommands
             game.util.numBrups = 0;
             game.util.numYrups = 0;
             game.util.hasMap = false;
+            game.util.hasCompass = false;
+            game.util.linkInd = true;
 
             //RE-PARSE ROOMS HERE
             game.collisionManager.ClearNotLink();
