71e8e38103b3e29e8cbd75ca03f43eaf63eb6535
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-04-21 19:51:36 -0400

    fixed small bug in OldMan room where if you unlocked door and triggered event tile again it would lock you in again

--- Classes/Collisions/CollisionScripts/LinkOnTile.cs
+++ Classes/Collisions/CollisionScripts/LinkOnTile.cs
@@ -84,10 +84,11 @@ namespace CSE3902_Game_Sprint0.Classes.Collisions.CollisionScripts
                     game.changeRoom(game.neighbors[game.util.roomNumber][1], direction);
                 }
             }
-            if(tile is EventTile && game.currentRoom.roomNumber == 8)
+            if(tile is EventTile && game.currentRoom.roomNumber == 8 && ((EventTile)tile).used == false)
             {
                 game.currentRoom.closeDoorways();
                 game.currentRoom.lockDoor(2);
+                ((EventTile)tile).used = true;
             }
             if (tile is PushableTile)
             {
