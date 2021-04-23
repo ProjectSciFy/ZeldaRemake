11f66847c1361cd672093c090d080bd12ba402bb
Gal Pinhasi <pinhasi.4@buckeyemail.osu.edu>
2021-04-22 23:46:20 -0400

    refactored background

---
+++ Classes/Level/BackgroundUtility/BackgroundStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE3902_Game_Sprint0.Classes.Level.BackgroundUtility
+{
+    public class BackgroundStorage
+    {
+        public int roomLimiter { get; set; } = 10;
+        public int drawOffset { get; set; } = 96;
+        public int specialRoomNumber { get; set; } = 16;
+        public BackgroundStorage()
+        {
+        }
+    }
+}
